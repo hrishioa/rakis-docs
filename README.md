399ca050bb0be43e8ae64c5fe6347be8ebcc304c
Hrishi Olickel <hrishioa@gmail.com>
2024-06-13 13:53:07 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Rakis - made with Lumentis
+
+`curl -fsSL https://bun.sh/install | bash # Install bun for macOS, Linux, and WSL`
+
+`bun install`
+
+`bun dev`
+
+Change things in `pages` to see the effect.
---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Rakis - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Rakis" />
+          <meta property="og:description" content="Provided is the core code, technical and personal writeup of Rakis, a project I'm building that's meant to provide browser-based decentrzlied inference as a network in a permissionless way." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
