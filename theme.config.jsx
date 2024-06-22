898ec1f00c852fc1e0df2565486a573d6df0b112
Hrishi Olickel <hrishioa@gmail.com>
2024-06-22 23:01:45 +1200

    Updated favicon and titles

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,31 +1,32 @@
 export default {
-      logo: <span>Rakis - made with Lumentis</span>,
-      editLink: {
-        component: null,
-      },
-      project: {
-        link: "https://github.com/hrishioa/lumentis",
-      },
-      feedback: {
-        content: null,
-      },
-      footer: {
-        text: (
-          <>
-            Made with 🫶 by&nbsp;
-            <a href="https://twitter.com/hrishioa" target="_blank">
-              Hrishi - say hi!
-            </a>
-          </>
-        ),
-      },
-      head: (
-        <>
-          <meta property="og:title" content="Rakis" />
-          <meta property="og:description" content="Provided is the core code, technical and personal writeup of Rakis, a project I'm building that's meant to provide browser-based decentrzlied inference as a network in a permissionless way." />
-          <meta name="robots" content="noindex, nofollow" />
-          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
-        </>
-      ),
-    };
-    
+  logo: <span>Rakis: Verifiable inference, peer-to-peer, in the browser.</span>,
+  editLink: {
+    component: null,
+  },
+  project: {
+    link: "https://github.com/hrishioa/rakis",
+  },
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: (
+      <>
+        Made with 🫶 by&nbsp;
+        <a href="https://twitter.com/Degen_Julian" target="_blank">
+          Julian - say hi!
+        </a>
+      </>
+    ),
+  },
+  head: (
+    <>
+      <meta property="og:title" content="Rakis" />
+      <meta
+        property="og:description"
+        content="Rakis is a peer to peer verifiable inference network that runs entirely in the browser."
+      />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    </>
+  ),
+};
